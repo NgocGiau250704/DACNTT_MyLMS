9a92f4554bb7b71db7b283f44da9ed3ad6a76409
NgocGiau250704 <167507769+NgocGiau250704@users.noreply.github.com>
2026-01-06 10:10:08 +0700

    Update submissionApi baseUrl to production URL

--- client/src/features/api/submissionApi.js
+++ client/src/features/api/submissionApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const submissionApi = createApi({
   reducerPath: "submissionApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8080/api/v1/submission",
+    baseUrl: "https://dacntt-mylms-1.onrender.com/api/v1/submission",
     credentials: "include", 
   }),
   
