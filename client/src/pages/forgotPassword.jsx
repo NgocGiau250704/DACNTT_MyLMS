3bef93a45da65ee270a05d4e01f872561b2d029d
NgocGiau250704 <167507769+NgocGiau250704@users.noreply.github.com>
2026-01-06 11:18:57 +0700

    Update forgot password API endpoint URL

--- client/src/pages/forgotPassword.jsx
+++ client/src/pages/forgotPassword.jsx
@@ -13,7 +13,7 @@ export default function ForgotPassword() {
     e.preventDefault();
     try {
       const res = await fetch(
-        "http://localhost:8080/api/v1/user/forgot-password",
+        "https://dacntt-mylms-1.onrender.com/api/v1/user/forgot-password",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
