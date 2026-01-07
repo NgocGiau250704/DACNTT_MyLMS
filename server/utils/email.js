3851d0bf45f13262869b3a0fe35883a89b16b6b4
NgocGiau250704 <167507769+NgocGiau250704@users.noreply.github.com>
2026-01-07 11:18:51 +0700

    Comment out email connection settings
    Comment out email configuration timeouts and TLS settings.

--- server/utils/email.js
+++ server/utils/email.js
@@ -19,12 +19,12 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  connectionTimeout: 20000, 
-  greetingTimeout: 20000,
-  socketTimeout: 20000,
-  tls: {
-    rejectUnauthorized: false
-  }
+  // connectionTimeout: 20000, 
+  // greetingTimeout: 20000,
+  // socketTimeout: 20000,
+  // tls: {
+  //   rejectUnauthorized: false
+  // }
 });
 
 async function sendVerificationEmail(toEmail, token){
